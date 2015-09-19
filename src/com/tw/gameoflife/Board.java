ae6d3940ecd64395398a091e42ed12a526175c7f
Monica Singh <monicas138@gmail.com>
2015-09-19 12:59:05 +0530

    added board and overloaded equals

---
+++ src/com/tw/gameoflife/Board.java
@@ -0,0 +1,15 @@
+package com.tw.gameoflife;
+
+public class Board {
+
+    @Override
+    public boolean equals( Object o ) {
+        if ( this == o ) {
+            return true;
+        }
+        if ( o == null || getClass() != o.getClass() ) {
+            return false;
+        }
+        return true;
+    }
+}
